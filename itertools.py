19c971e8475a1835c9a44c74fdaac911d7a1a4bb
WonJun <c68254@gmail.com>
2024-02-03 13:41:48 +0900

    chores

--- itertools.py
+++ itertools.py
@@ -1,10 +1,10 @@
-# from itertools import permutations
-#
-# data = ['A', 'B', 'C']
-# result = list(permutations(data, 3))
-#
-# print(result)
-#
+from itertools import permutations
+
+data = ['A', 'B', 'C']
+result = list(permutations(data, 3))
+
+print(result)
+
 # from itertools import combinations
 #
 # data = ['A', 'B', 'C']
